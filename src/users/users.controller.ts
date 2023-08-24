88c206ef10d10c125b21cf6d4e0d1f99d34b5450
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-08-24 17:36:23 +0100

    user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDTO } from 'src/dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('api/v1/users')
+export class UsersController {
+  @Get()
+  getAllUsers() {
+    return 'get all users';
+  }
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDTO) {
+    return 'user created';
+  }
+}
