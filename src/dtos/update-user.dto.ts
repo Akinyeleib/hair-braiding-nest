22e15f0426a7da90ad6355263ab7adb11495dbf6
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-08-25 06:52:19 +0100

    update-user-dto file created

---
+++ src/dtos/update-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsStringNotEmpty } from 'src/utils/helper';
+
+export class UpdateUserDTO {
+  @IsStringNotEmpty() username: string
+  @IsEmail() email: string
+  @IsStringNotEmpty() address: string
+  @IsStringNotEmpty() phone_number: string
+  @IsStringNotEmpty() state: string
+  @IsStringNotEmpty() country: string
+  @IsStringNotEmpty() role: string
+  @IsStringNotEmpty() gender: string
+  @IsStringNotEmpty() password1: string
+  @IsStringNotEmpty() password2: string
+}
